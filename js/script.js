0cc57904f64ed0ff6f07083e9b657f221238b776
saidtalfana <saidtalfana98@gmail.com>
2024-03-28 15:13:09 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
+const apiKey = '29524cbd82c2daa726175f3d9ed38508';
+const inputSearch = document.querySelector('.search_input');
+const btn = document.querySelector('.search_button');
+const icon = document.querySelector('.icons');
 
-const api = "https://api.openweathermap.org/data/2.5/weather?q=fes&appid=29524cbd82c2daa726175f3d9ed38508&units=metric"
- async function get(){
-const response = await fetch (api);
-const data = await response.json();
+async function get(city) {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(api);
+    const data = await response.json();
+    console.log(city);
+    document.querySelector('.cityName').innerHTML = data.name;
+    document.querySelector('.Temperature').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.hum').innerHTML = Math.round(data.main.humidity) + "%";
+    document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + "km/h";
 
-console.log(data )
-document.querySelector('.cityName').innerHTML = data.name;
-document.querySelector('.Temperature').innerHTML = data.main.temp;
-document.querySelector('.hum').innerHTML = data.main.humidity;
-document.querySelector('.wind').innerHTML = data.wind.speed;
+    if(data.weather[0].main =="Clouds"){
+      icon.src = "./image/clou"
+
+    } else if(data.weather[0].main =="clear"){
+      icon.src = "./image/clear.png"
+    }else if(data.weather[0].main =="Rain"){
+      icon.src = "./image/raing.png"
+    }else if(data.weather[0].main =="Drizzle"){
+      icon.src = "./image/drizzle.png"
+    }else if(data.weather[0].main =="Mist"){
+      icon.src = "./image/mist.png"
+    }
+
 }
-get()
+
+btn.addEventListener('click', () => {
+    get(inputSearch.value);
+});
